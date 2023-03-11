4b0c383796bd8290d353bba608ce9b6e1d03d060
syun <2dgames.jp@gmail.com>
2023-03-11 16:55:54 +0900

    敵の左右移動を実装

--- gd_actiontest2/Enemy.gd
+++ gd_actiontest2/Enemy.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 class_name Enemy
 
+const MOVE_SPEED = 180
 const GRAVITY = 30;
 
 enum eState {
@@ -17,6 +18,8 @@ var _state = eState.STAND_BY
 var _timer = 0.0
 var _cnt = 0
 var _velocity = Vector2.ZERO
+var _move_speed = 0.0
+var _is_left = true
 
 func vanish() -> void:
 	if _state == eState.VANISH:
@@ -40,12 +43,23 @@ func _physics_process(delta: float) -> void:
 			var d = target - position
 			if d.length() < 600:
 				d = d.normalized()
-				_velocity.x = d.x * 100
+				_velocity.x = d.x * MOVE_SPEED
+				_move_speed = _velocity.x
+				_is_left = true
 				_state = eState.WALK
 		eState.WALK:
 			set_velocity(_velocity)
 			move_and_slide()
 			_velocity = velocity
+			if is_on_wall():
+				# 壁ぶつかった.
+				_is_left = _is_left == false
+				_spr.flip_h = _is_left == false
+				if _is_left:
+					_velocity.x = _move_speed
+				else:
+					_velocity.x = -_move_speed
+			
 			_spr.offset.y = ((_cnt/8)%2) * 2
 		eState.VANISH:
 			_timer += delta
